4145272d48288462816c4425ca1fd6bb7ce9f285
IsalanyaMwaseSam <eazikezi1999@gmail.com>
2023-09-17 13:13:31 +0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { MailchimpForm } from "./MailchimpForm";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import whatsappIcon from '../assets/img/whatsappIcon.svg';
+import githubIcon from '../assets/img/githubIcon.svg';
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <MailchimpForm />
+          <Col size={12} sm={6}>
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="#"><img src={navIcon1} alt="Icon" /></a>
+              {/* <a href="#"><img src={navIcon2} alt="Icon" /></a> */}
+              <a href="tel:+256705876748"><img src={whatsappIcon} alt="" /></a>
+              <a href="https://github.com/IsalanyaMwaseSam"><img src={githubIcon} alt="" /></a>
+            </div>
+            <p>Copyright 2022. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
